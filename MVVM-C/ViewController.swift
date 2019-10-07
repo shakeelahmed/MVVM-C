f1c00ca76b76f1df45f1052526537c5018ef00e7
Shakeel Ahmed <shakeelseny87@gmail.com>
2019-10-07 16:21:32 +0500

    New project created

---
+++ MVVM-C/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-C
+//
+//  Created by Shakeel Ahmad on 07/10/2019.
+//  Copyright © 2019 Shakeel Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
